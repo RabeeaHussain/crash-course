fba284128d6da8ff799f2daf7036e83d66ab3841
MrLongBeard <umair.hussain96@gmail.com>
2020-06-27 02:52:40 +0500

    [feat] Added prompt for year input

---
+++ js basic/jv practise/static/javascript/basic.js
@@ -0,0 +1,11 @@
+//Challenge 1:You Age in Days
+function Ageindays(){
+    var birthyear=prompt('What year were you born ?');
+    var ageindays=(2019 - birthyear) * 365;
+    var h1=document.createElement('h1')
+    var textAnswer=document.createTextNode('You are' + ageindays + 'days old');
+    h1.setAttribute('id','Ageindays');
+    h1.appendChild(textAnswer)
+    document.getElementById('flex-box-result').appendChild(h1);        
+}
+
